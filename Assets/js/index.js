f7d3d7533bf2d42e373d0ba0e1ae9a903efd64e0
izsie <izsie.robinson@gmail.com>
2020-06-24 12:39:14 -0500

    Added top section and navigation bar

---
+++ Assets/js/index.js
@@ -0,0 +1,10 @@
+let meet1 = getElementById('meet-pranav1');
+let donate1 = getElementById('donate1');
+
+function goAbout() {
+    window.location.href='../../Pages/about.html';
+}
+
+function goDonate() {
+    window.location.href='https://secure.actblue.com/';
+}
